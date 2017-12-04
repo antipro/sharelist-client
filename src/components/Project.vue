45dea980c60c65d52830221b455d2fabecca0081
antipro <antipro@163.com>
2017-12-04 11:05:53 +0800

    123

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -24,7 +24,7 @@
           </div>
           <div class="form-group">
             <div class="input-group">
-              <input type="tel" class="form-control" placeholder="输入要分享的账号并添加..." v-model="tel" @keyup.enter="addShare">
+              <input type="tel" class="form-control" placeholder="输入要分享的账号并添加..." v-model="tel" @keyup.stop.enter="addShare">
               <span class="input-group-btn">
                 <button class="btn btn-default" type="button" @click="addShare">
                   <span class="glyphicon glyphicon-plus"></span>
@@ -112,6 +112,7 @@ export default {
       this.tel = ''
     },
     updateProject () {
+      console.log('update')
       this.$root.updateProject(this.pid, this.pname, this.shares)
       this.$router.go(-1)
     }
